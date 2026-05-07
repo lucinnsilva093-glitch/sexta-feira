509e636f2bd37ad081f1fe13ed7f518816ed72d6
lucinnsilva093-glitch <lucinnsilva093@gmail.com>
2026-05-07 20:00:23 -0300

    Update testesdiversos.py

--- testesdiversos.py
+++ testesdiversos.py
@@ -4,29 +4,85 @@ import requests
 import os
 
 # ================== CONFIG ==================
-API_KEY = "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"
-print("API_KEY =", repr(API_KEY))
+API_KEY = os.environ["API_KEY"]
+
+print("CHAVE =", repr(API_KEY))
+
 # ================== APP ==================
 app = Flask(__name__)
 CORS(app)
 
-# ================== ROTA ==================
+# ================== HOME ==================
 @app.route("/")
 def home():
-    return "🔥 TESTE NOVO 123 🔥"
+    return "🤖 Sexta-Feira online"
 
 # ================== CHAT ==================
 @app.route("/chat", methods=["POST"])
 def chat():
+    try:
 
-    data = request.get_json()
+        # recebe mensagem do frontend
+        data = request.get_json()
 
-    mensagem = data["message"]
+        mensagem = data["message"]
 
-    return jsonify({
-        "response": "Recebi: " + mensagem
-    })
+        # envia para OpenRouter
+        resposta = requests.post(
+            "https://openrouter.ai/api/v1/chat/completions",
+
+            headers={
+                "Authorization": f"Bearer {API_KEY.strip()}",
+                "HTTP-Referer": "https://sextafeira-lc.netlify.app",
+                "X-Title": "Sexta-Feira",
+                "Content-Type": "application/json"
+            },
+
+            json={
+                "model": "meta-llama/llama-3-8b-instruct",
+
+                "messages": [
+                    {
+                        "role": "system",
+                        "content": (
+                            "Você é Sexta-Feira, "
+                            "uma assistente virtual feminina, "
+                            "simpática, inteligente e futurista. "
+                            "Sempre responda em português do Brasil."
+                        )
+                    },
+
+                    {
+                        "role": "user",
+                        "content": mensagem
+                    }
+                ]
+            }
+        )
+
+        # transforma em json
+        resposta_json = resposta.json()
+
+        # mostra nos logs
+        print(resposta_json)
+
+        # pega resposta da IA
+        texto = resposta_json["choices"][0]["message"]["content"]
+
+        # envia pro frontend
+        return jsonify({
+            "response": texto
+        })
+
+    except Exception as e:
+
+        print("ERRO:", e)
+
+        return jsonify({
+            "response": str(e)
+        })
+
 # ================== RUN ==================
 if __name__ == "__main__":
-    print("🤖 Sexta-Feira online...")
+    print("🤖 Sexta-Feira iniciando...")
     app.run(debug=True)
