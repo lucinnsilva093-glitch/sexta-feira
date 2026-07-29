f166263581330ab699bd4570c973faf527b34110
lucinnsilva093-glitch <lucinnsilva093@gmail.com>
2026-07-29 14:18:54 -0300

    Update memoria.py

--- memoria.py
+++ memoria.py
@@ -4,22 +4,21 @@ from datetime import datetime
 class Memoria:
 
     def buscar_historico(self, session_id):
-
         return carregar_historico(session_id)
+
+    def salvar(self, session_id, pergunta, resposta):
         agora = datetime.now().isoformat()
-    def salvar(
 
-   salvar_mensagem(
-       session_id,
-       "user",
-       pergunta,
-       agora
-   )
+        salvar_mensagem(
+            session_id,
+            "user",
+            pergunta,
+            agora
+        )
 
-   salvar_mensagem(
-       session_id,
-       "assistant",
-       resposta,
-       agora
-   )
-    )
+        salvar_mensagem(
+            session_id,
+            "assistant",
+            resposta,
+            agora
+        )
